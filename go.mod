9534cd47967e67079d160e99703c08be675a0724
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2024-08-13 01:13:09 +0900

    We no longer need Go....

--- go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/miscord-dev/kubevctx
-
-go 1.22.2
-
-require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-)
--- pkg/vctx/hook/zsh.sh
+++
@@ -1,9 +0,0 @@
-_kubevctx_hook() {
-  trap -- '' SIGINT;
-  eval "$("kubevctx" export zsh)";
-  trap - SIGINT;
-}
-typeset -ag precmd_functions;
-if [[ -z "${precmd_functions[(r)_kubevctx_hook]+1}" ]]; then
-  precmd_functions=( _kubevctx_hook ${precmd_functions[@]} )
-fi
